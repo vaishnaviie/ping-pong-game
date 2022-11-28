cc31ad7b3f3b34656d17c456f8ec9a8e7f4581fe
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-11-28 13:22:36 +0530

    update: ball is now moving

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 const canvas=document.getElementById("canvas");
 const ctx= canvas.getContext("2d");
-// var ctx = c.getContext("2d");
 
 canvas.width=window.innerWidth;
 canvas.height=window.innerHeight;
 
+let ballX=200;
+let ballY=200;
+let ballRadius=20;
+let ballVelocityX=2;
+let ballVelocityY=2;
 
-ctx.fillStyle= "rgb(203, 47, 172)";
-ctx.strokeStyle="rgb(203, 47, 172)";
-ctx.beginPath();
-ctx.arc(100,200,20,0,Math.PI*2);
-ctx.stroke();
-ctx.fill();
-ctx.stroke();
+function gameUpdate(){
+    ballX+=ballVelocityX;
+    ballY+=ballVelocityY;
+}
+
+function gameDraw(){
+    ctx.fillStyle= "rgb(203, 47, 172)";
+    ctx.strokeStyle="rgb(203, 47, 172)";
+    ctx.beginPath();
+    ctx.arc(ballX,ballY,ballRadius,0,Math.PI*2);
+    ctx.stroke();
+    ctx.fill();
+    ctx.stroke();
+}
+
+function gameLoop(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    window.requestAnimationFrame(gameLoop);
+    gameUpdate();
+    gameDraw();
+}
+
+gameLoop();
+
+
 
