78457157738972a779a243b71b703a35829b3201
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-11-29 13:58:00 +0530

    update: created the paddles

--- script.js
+++ script.js
@@ -34,6 +34,25 @@ class Ball{
     }
 };
 
+class Paddle{
+    constructor(pos,velocity,width,height){
+        this.pos=pos;
+        this.velocity=velocity;
+        this.width=width;
+        this.height=height;
+
+        this.update=function(){
+            
+        }
+
+        this.draw=function(){
+            ctx.fillStyle="rgb(203, 47, 172)";
+            ctx.fillRect(this.pos.x,this.pos.y,this.width,this.height);
+            // ctx.fill();
+        }
+    }
+};
+
 function ballCollusionWithEdges(ball){
     if(ball.pos.y+ball.radius>=canvas.height){
         ball.velocity.y*=-1;
@@ -50,17 +69,20 @@ function ballCollusionWithEdges(ball){
 }
 
 const ball=new Ball(xy(500,250),xy(15,15),20);
+const Paddle1=new Paddle(xy(100,canvas.height-30),xy(16,16),200,30);
+const Paddle2=new Paddle(xy(500,0),xy(16,16),200,30);
 
 function gameUpdate(){
-
     ball.update();
     ballCollusionWithEdges(ball);
+    Paddle1.update();
 
 }
 
 function gameDraw(){
     ball.draw();
-
+    Paddle1.draw();
+    Paddle2.draw();
 }
 
 function gameLoop(){
