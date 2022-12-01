4de4a6cf7c18f4aae3ab99d0b6939bdeae6293a8
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-12-01 23:33:08 +0530

    update: score functionality

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ class Paddle{
         this.velocity=velocity;
         this.width=width;
         this.height=height;
+        this.score=0;
 
         this.update=function(){
             if(keyPressed[RIGHT]){
@@ -70,15 +71,15 @@ class Paddle{
 };
 
 function ballCollusionWithEdges(ball){
-    if(ball.pos.y+ball.radius>=canvas.height){
-        ball.velocity.y*=-1;
-    }
+    // if(ball.pos.y+ball.radius>=canvas.height){
+    //     ball.velocity.y*=-1;
+    // }
     if(ball.pos.x+ball.radius>=canvas.width){
         ball.velocity.x*=-1;
     }
-    if(ball.pos.y<=0){
-        ball.velocity.y*=-1;
-    }
+    // if(ball.pos.y<=0){
+    //     ball.velocity.y*=-1;
+    // }
     if(ball.pos.x<=0){
         ball.velocity.x*=-1;
     }
@@ -112,6 +113,34 @@ function player2(ball,Paddle){
     }
 }
 
+function responseball(ball){
+    if(ball.velocity.y<0){
+        ball.pos.x=(Math.random()*canvas.width-200)+100;
+        ball.pos.y=150;
+    }
+    if(ball.velocity.y>0){
+        ball.pos.x=(Math.random()*canvas.width-200)+100;
+        ball.pos.y=canvas.height-150;
+    }
+
+    ball.velocity.x*=-1;
+    ball.velocity.y*=-1;
+}
+
+function increaseScore(ball,Paddle1,Paddle2){
+    if(ball.pos.y<=-ball.radius){
+        Paddle1.score+=1;
+        document.getElementById("score-player1").innerHTML=Paddle1.score;
+        responseball(ball);
+    }
+
+    if(ball.pos.y>=canvas.height+ball.radius){
+        Paddle2.score+=1;
+        document.getElementById("score-player2").innerHTML=Paddle2.score;
+        responseball(ball);
+    }
+}
+
 const ball=new Ball(xy(500,250),xy(15,15),20);
 const Paddle1=new Paddle(xy(100,canvas.height-30),xy(16,16),200,30);
 const Paddle2=new Paddle(xy(500,0),xy(30,30),200,30);
@@ -123,6 +152,7 @@ function gameUpdate(){
     PaddleCollusionWithEdges(Paddle1);
     PaddleCollusionWithEdges(Paddle2);
     player2(ball,Paddle2);
+    increaseScore(ball,Paddle1,Paddle2);
 }
 
 function gameDraw(){
