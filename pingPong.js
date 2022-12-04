06e31593bd5277b6d547187ffe2b3f02347943dd
Vaishnavi P <75312513+vaishnaviie@users.noreply.github.com>
2022-12-04 21:35:34 +0530

    fix: timing

--- pingPong.js
+++ pingPong.js
@@ -169,7 +169,7 @@ function heart(heartLeft){
         function pageRedirect() {
             window.location.replace("gameOver.html");
         }      
-        setTimeout(pageRedirect(),1000);
+        setTimeout(pageRedirect(),500);
     }
 }
 
